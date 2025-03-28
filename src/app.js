c7e4f30e7e4a8174c38edbff39837b9b4d45912b
DANY <vermenrainer@gmail.com>
2025-03-28 15:48:49 -0500

    first commit - web server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/* Importamos al framework express */
+import express from "express";
+
+/* asignamos a app toda funcinalidad para mi server web */
+const app = express();
+
+/* setear un puerto a mi web server */
+app.set("port",5000)
+
+/* hacemos disponible a mi server app para toda la aplicacion */
+export default app;
