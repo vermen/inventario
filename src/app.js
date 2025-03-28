bcac645ef9002b0923c303afd20fbde29741b02c
DANY <vermenrainer@gmail.com>
2025-03-28 16:28:32 -0500

    enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 /* Importamos al framework express */
 import express from "express";
-
+import categoriaRoutes from "./routes/categorias.routes.js"
 /* asignamos a app toda funcinalidad para mi server web */
 const app = express();
 
 /* setear un puerto a mi web server */
 app.set("port",5000)
+
+/* routes */
+app.use("/api/categorias",categoriaRoutes)
 
 /* hacemos disponible a mi server app para toda la aplicacion */
 export default app;
---
+++ src/routes/categorias.routes.js
@@ -0,0 +1,10 @@
+import {Router} from "express";
+import { methodHTTP as categoriaController } from "../controllers/categoria.controllers.js";
+/* creamos el enrutador */
+const router = Router();
+
+/* configuramos repsuesta desde server metodo http get */
+router.get("/", categoriaController.getCategorias )
+
+/* hacemos disponible a router en toda la app */
+export default router;
